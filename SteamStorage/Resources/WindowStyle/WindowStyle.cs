992063ab708ad15d4b3c40b644562941bd00b84e
Owner <Owner@DESKTOP-IHPTVB7>
2023-05-31 22:19:02 +0300

    upd

---
+++ SteamStorage/Resources/WindowStyle/WindowStyle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SteamStorage.Resources.WindowStyle
+{
+    public partial class WindowStyle
+    {
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            (sender as Window).StateChanged += Window_StateChanged;
+        }
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            Window me = sender as Window;
+            Button maximizeCaptionButton = me.Template.FindName("MaxRestoreButton", me) as Button;
+            maximizeCaptionButton.Content = me.WindowState == WindowState.Maximized ? "2" : "1";
+
+        }
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((sender as FrameworkElement).TemplatedParent as Window).Close();
+        }
+        private void MaxRestoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((sender as FrameworkElement).TemplatedParent as Window)
+                .WindowState = (((sender as FrameworkElement).TemplatedParent as Window)
+                .WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+        }
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((sender as FrameworkElement).TemplatedParent as Window)
+                .WindowState = WindowState.Minimized;
+        }
+    }
+}
