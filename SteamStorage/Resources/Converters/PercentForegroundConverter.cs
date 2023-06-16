fc05202699b09c05735b843ca2ce2756990540bb
Artem <artemdem972@gmail.com>
2023-06-16 19:49:35 +0300

    upd

--- SteamStorage/Resources/Converters/PercentForegroundConverter.cs
+++ SteamStorage/Resources/Converters/PercentForegroundConverter.cs
@@ -3,6 +3,7 @@ using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace SteamStorage.Resources.Converters
 {
@@ -12,8 +13,9 @@ namespace SteamStorage.Resources.Converters
         {
             if (value is null) return value;
             double percent = (double)value;
-            if (parameter is not null && ((string)parameter) == "Color") return Dictionaries.GetColor(percent < 0 ? "PercentMinusColor" : "PercentPlusColor");
-            return Dictionaries.GetSolidColorBrush(percent < 0 ? "PercentMinus" : "PercentPlus");
+            SolidColorBrush solidColorBrush = Dictionaries.GetSolidColorBrush(percent < 0 ? "PercentMinus" : "PercentPlus");
+            if (parameter is not null && ((string)parameter) == "Color") return solidColorBrush.Color;
+            return solidColorBrush;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
