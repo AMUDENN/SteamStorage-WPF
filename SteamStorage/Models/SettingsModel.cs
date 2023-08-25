538aeb6e9b04c39b3356f8b70b3b16773d2a9568
Artem <artemdem972@gmail.com>
2023-08-25 22:29:48 +0300

    upd

--- SteamStorage/Models/SettingsModel.cs
+++ SteamStorage/Models/SettingsModel.cs
@@ -1,13 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using OfficeOpenXml;
 using SteamStorage.Utilities;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using OfficeOpenXml;
-using System.Drawing;
-using OfficeOpenXml.Style;
-using System;
-using System.IO.Packaging;
-using System.Linq.Expressions;
+using System.Linq;
 
 namespace SteamStorage.Models
 {
@@ -144,7 +142,109 @@ namespace SteamStorage.Models
                 {
                     var remainWorksheet = package.Workbook.Worksheets.Add("Остатки");
 
+                    remainWorksheet.Cells["A1"].Value = "Название";
+                    remainWorksheet.Cells["B1"].Value = "Количество";
+                    remainWorksheet.Cells["C1"].Value = "Цена покупки";
+                    remainWorksheet.Cells["D1"].Value = "Сумма";
+                    remainWorksheet.Cells["E1"].Value = "Дата покупки";
+                    remainWorksheet.Cells["F1"].Value = "Текущая цена";
+                    remainWorksheet.Cells["G1"].Value = "Дата обновления";
+                    remainWorksheet.Cells["H1"].Value = "Изменение (%)";
+                    remainWorksheet.Cells["I1"].Value = "Ссылка";
+                    remainWorksheet.Cells["A1:I1"].Style.Font.Bold = true;
+                    remainWorksheet.Cells["A1:I1"].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+
+                    List<RemainElementModel> remains = _context.RemainElementModels.ToList();
+
+                    int i = 2;
+                    foreach (var item in remains)
+                    {
+                        remainWorksheet.Cells[i, 1].Value = item.Title;
+                        remainWorksheet.Cells[i, 2].Value = item.Count;
+                        remainWorksheet.Cells[i, 3].Value = item.CostPurchase;
+                        remainWorksheet.Cells[i, 4].Value = item.AmountPurchase;
+                        remainWorksheet.Cells[i, 5].Value = item.DatePurchase.ToString(Constants.DateFormat);
+                        remainWorksheet.Cells[i, 6].Value = item.LastCost;
+                        remainWorksheet.Cells[i, 7].Value = item.DateLastUpdate.ToString(Constants.DateFormat);
+                        remainWorksheet.Cells[i, 8].Value = (item.Percent > 0 ? "+" : "") + Math.Round(item.Percent, 2);
+                        remainWorksheet.Cells[i, 9].Value = item.Url;
+                        i++;
+                    }
+
+                    remainWorksheet.Cells[i, 2].Value = "Общее количество";
+                    remainWorksheet.Cells[i, 3].Value = "Средняя цена покупки";
+                    remainWorksheet.Cells[i, 4].Value = "Общая сумма покупки";
+                    remainWorksheet.Cells[i, 6].Value = "Средняя текущая цена";
+                    remainWorksheet.Cells[i, 7].Value = "Общая текущая стоимость";
+                    remainWorksheet.Cells[i, 8].Value = "Общее изменение";
+
+                    remainWorksheet.Cells[i + 1, 1].Value = "Итого";
+                    remainWorksheet.Cells[i + 1, 2].Value = CalculationModel.GetRemainTotalCount(remains);
+                    remainWorksheet.Cells[i + 1, 3].Value = Math.Round(CalculationModel.GetRemainAverageCostPurchase(remains), 2);
+                    remainWorksheet.Cells[i + 1, 4].Value = CalculationModel.GetRemainTotalAmountPurchase(remains);
+                    remainWorksheet.Cells[i + 1, 6].Value = Math.Round(CalculationModel.GetRemainAverageCurrentCost(remains), 2);
+                    remainWorksheet.Cells[i + 1, 7].Value = CalculationModel.GetRemainTotalCurrentAmount(remains);
+                    remainWorksheet.Cells[i + 1, 8].Value = Math.Round(CalculationModel.GetRemainAveragePercent(remains), 2);
+
+                    remainWorksheet.Cells[i, 1, i + 1, 8].Style.Font.Bold = true;
+                    remainWorksheet.Cells[i, 1, i + 1, 8].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+
+                    remainWorksheet.Cells[1, 1, i + 1, 9].AutoFitColumns();
+
+
+
                     var archiveWorksheet = package.Workbook.Worksheets.Add("Архив");
+
+                    archiveWorksheet.Cells["A1"].Value = "Название";
+                    archiveWorksheet.Cells["B1"].Value = "Количество";
+                    archiveWorksheet.Cells["C1"].Value = "Цена покупки";
+                    archiveWorksheet.Cells["D1"].Value = "Сумма покупки";
+                    archiveWorksheet.Cells["E1"].Value = "Дата покупки";
+                    archiveWorksheet.Cells["F1"].Value = "Цена продажи";
+                    archiveWorksheet.Cells["G1"].Value = "Сумма продажи";
+                    archiveWorksheet.Cells["H1"].Value = "Дата продажи";
+                    archiveWorksheet.Cells["I1"].Value = "Изменение (%)";
+                    archiveWorksheet.Cells["J1"].Value = "Ссылка";
+                    archiveWorksheet.Cells["A1:J1"].Style.Font.Bold = true;
+                    archiveWorksheet.Cells["A1:J1"].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+
+                    List<ArchiveElementModel> archives = _context.ArchiveElementModels.ToList();
+
+                    int j = 2;
+                    foreach (var item in archives)
+                    {
+                        archiveWorksheet.Cells[j, 1].Value = item.Title;
+                        archiveWorksheet.Cells[j, 2].Value = item.Count;
+                        archiveWorksheet.Cells[j, 3].Value = item.CostPurchase;
+                        archiveWorksheet.Cells[j, 4].Value = item.AmountPurchase;
+                        archiveWorksheet.Cells[j, 5].Value = item.DatePurchase.ToString(Constants.DateFormat);
+                        archiveWorksheet.Cells[j, 6].Value = item.CostSold;
+                        archiveWorksheet.Cells[j, 7].Value = item.AmountSold;
+                        archiveWorksheet.Cells[j, 8].Value = item.DateSold.ToString(Constants.DateFormat);
+                        archiveWorksheet.Cells[j, 9].Value = (item.Percent > 0 ? "+" : "") + Math.Round(item.Percent, 2);
+                        archiveWorksheet.Cells[j, 10].Value = item.Url;
+                        j++;
+                    }
+
+                    archiveWorksheet.Cells[j, 2].Value = "Общее количество";
+                    archiveWorksheet.Cells[j, 3].Value = "Средняя цена покупки";
+                    archiveWorksheet.Cells[j, 4].Value = "Общая сумма покупки";
+                    archiveWorksheet.Cells[j, 6].Value = "Средняя цена продажи";
+                    archiveWorksheet.Cells[j, 7].Value = "Общая сумма продажи";
+                    archiveWorksheet.Cells[j, 9].Value = "Общее изменение";
+
+                    archiveWorksheet.Cells[j + 1, 1].Value = "Итого";
+                    archiveWorksheet.Cells[j + 1, 2].Value = CalculationModel.GetArchiveTotalCount(archives);
+                    archiveWorksheet.Cells[j + 1, 3].Value = Math.Round(CalculationModel.GetArchiveAverageCostPurchase(archives), 2);
+                    archiveWorksheet.Cells[j + 1, 4].Value = CalculationModel.GetArchiveTotalAmountPurchase(archives);
+                    archiveWorksheet.Cells[j + 1, 6].Value = Math.Round(CalculationModel.GetArchiveAverageCostSold(archives), 2);
+                    archiveWorksheet.Cells[j + 1, 7].Value = CalculationModel.GetArchiveTotalAmountSold(archives);
+                    archiveWorksheet.Cells[j + 1, 9].Value = Math.Round(CalculationModel.GetArchiveAveragePercent(archives), 2);
+
+                    archiveWorksheet.Cells[j, 1, j + 1, 9].Style.Font.Bold = true;
+                    archiveWorksheet.Cells[j, 1, j + 1, 9].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+
+                    archiveWorksheet.Cells[1, 1, j + 1, 10].AutoFitColumns();
 
                     byte[] data = package.GetAsByteArray();
 
