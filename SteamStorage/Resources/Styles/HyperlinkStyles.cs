61aa2da462752b01666afcdbe5dd6752925fb424
Artem <artemdem972@gmail.com>
2023-08-26 23:32:44 +0300

    upd

---
+++ SteamStorage/Resources/Styles/HyperlinkStyles.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+
+namespace SteamStorage.Resources.Styles
+{
+    public partial class HyperlinkStyles
+    {
+        private void BrowserHyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is not Hyperlink) return;
+            Hyperlink hyperlink = (Hyperlink)sender;
+            var sInfo = new System.Diagnostics.ProcessStartInfo(hyperlink.NavigateUri.AbsoluteUri)
+            {
+                UseShellExecute = true,
+            };
+            System.Diagnostics.Process.Start(sInfo);
+        }
+    }
+}
